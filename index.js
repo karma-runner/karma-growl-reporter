e9ea8fc1ecf095608156612cb834d20a4185a668
David Miani <davidmiani@gmail.com>
2015-11-14 19:54:02 +1000

    Added separate labels for different notifications
    This allows the different types of message to be handled differently
    by the user's growl notification client (e.g making failure messages
    red, success green, etc).

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ var MSG_ERROR = '';
 var OPTIONS = {
   success: {
     dispname: 'Success',
+    label: 'success',
     title: 'PASSED - %s',
     icon: path.join(__dirname, 'images/success.png')
   },
   failed: {
     dispname: 'Failure',
+    label: 'failure',
     title: 'FAILED - %s',
     icon: path.join(__dirname, 'images/failed.png')
   },
   error: {
     dispname: 'Aborted',
+    label: 'aborted',
     title: 'ERROR - %s',
     icon: path.join(__dirname, 'images/error.png')
   }
@@ -33,7 +36,10 @@ var GrowlReporter = function(helper, logger, config) {
     return helper.merge(OPTIONS[type], {title: prefix + util.format(OPTIONS[type].title, browser)});
   };
 
-  growly.register('Karma', '', [], function(error) {
+  var allNotifications = Object.keys(OPTIONS).map(function(key) {
+    return OPTIONS[key];
+  });
+  growly.register('Karma', '', allNotifications, function(error) {
     var warning = 'No running version of GNTP found.\n' +
 	                'Make sure the Growl service is installed and running.\n' +
                   'For more information see https://github.com/theabraham/growly.';
